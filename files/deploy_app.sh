d5e2d60fe1347d6462849a84a6ab137827309e38
trevorkaler-csi <103516395+trevorkaler-csi@users.noreply.github.com>
2022-04-12 14:35:38 -0500

    Update deploy_app.sh
    Added some flair

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><iframe src="https://giphy.com/embed/51Uiuy5QBZNkoF3b2Z" width="477" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/mrw-boy-51Uiuy5QBZNkoF3b2Z">via GIPHY</a></p></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to Trevor's app, where we do it live.
   <!-- END -->
   
   </div>
